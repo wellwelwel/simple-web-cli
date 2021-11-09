f6ef3074cf6a44126f2436cffb2f1e2948bc5fd6
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2021-11-09 05:05:19 -0300

    - bugfixes: delete DS_Store only on macOS

--- .webservice/modules/deleteDS_Store.js
+++ .webservice/modules/deleteDS_Store.js
@@ -4,7 +4,7 @@ const exec = require('./execShellCommand');
 
 module.exports = async () => {
 
-   if (!process.platform === 'darwin') return;
+   if (process.platform !== 'darwin') return;
 
    await exec('find . -name ".DS_Store" -type f -delete');
 };
