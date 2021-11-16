2151130cd5725cb8759cd361d2ae686708204f36
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2021-11-16 02:15:19 -0300

    Copy required resources

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,45 @@
 #! /usr/bin/env node
-require('../.web/tasks/start');
+(async () => {
+
+   const fs = require('fs');
+   const { EOL } = require('os');
+   const requires = {
+
+      dirs: [
+
+         '.library'
+      ],
+      files: [
+
+         '.web-config.json',
+         '.web-replace.json',
+         '.babelrc',
+         '.eslintrc.js'
+      ]
+   };
+
+   requires.dirs.forEach(require => {
+
+      if (!fs.existsSync(`./${require}`)) fs.mkdirSync(`./${require}`);
+   });
+
+   requires.files.forEach(async require => {
+
+      if (!fs.existsSync(`./${require}`)) await fs.copyFile(`../${require}`, `./${require}`);
+   });
+
+   if (!fs.existsSync('./.gitignore')) await fs.copyFile('../.gitignore', './.gitignore');
+   else {
+
+      const toIgnore = [
+
+         '.main',
+         '.release',
+         'src/exit'
+      ];
+
+      fs.appendFileSync('./.gitignore', `${EOL}${toIgnore.join(EOL)}`);
+   }
+   
+   require('../.web/tasks/start');
+})();
