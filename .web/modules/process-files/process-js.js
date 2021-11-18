deedec6c6ed55a470ee1e6a3a15026a4714c873b
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2021-11-18 19:42:33 -0300

    Handling errors when importing by local modules

--- .web/modules/process-files/process-js.js
+++ .web/modules/process-files/process-js.js
@@ -16,49 +16,57 @@ const postProcess = require('./post-process-replace');
 const requiredResources = process_files.js.require;
 const packageName = JSON.parse(_fs.readFileSync('.library/package.json', 'utf8'));
 
-async function recursive_require(file, replace) {
+async function recursive_require(file, replace, log) {
 
    let content = await postProcess({ src: file, response: true, local: replace });
    const requireds = content.match(/require\(.*?\);/gim);
    
    for (const required in requireds) {
 
-      let fixPath = requireds[required].replace(/\.\.\//gim, '').replace('./', '');
-      const origins = requiredResources.split(sep);
-      if (origins.length > 1) origins.forEach(folder => fixPath = fixPath.replace(folder, ''));
-      else fixPath = fixPath.replace(requiredResources, '');
+      try {
 
-      const regex = /(require\(['"`])(.+?)(['"`]\);)/;
-      const requiredName = regex.exec(fixPath)[2].replace(RegExp(packageName.name.replace(/\//, '\\/'), 'gim'), '');
-      const exist_require = () => {
+         let fixPath = requireds[required].replace(/\.\.\//gim, '').replace('./', '');
+         const origins = requiredResources.split(sep);
+         if (origins.length > 1) origins.forEach(folder => fixPath = fixPath.replace(folder, ''));
+         else fixPath = fixPath.replace(requiredResources, '');
+   
+         const regex = /(require\(['"`])(.+?)(['"`]\);)/;
+         const requiredName = regex.exec(fixPath)[2].replace(RegExp(packageName.name.replace(/\//, '\\/'), 'gim'), '');
+         const exist_require = () => {
+   
+            const required_path = normalize(`${requiredResources}${sep}${requiredName}`);
+   
+            if (_fs.existsSync(`${required_path}${sep}index.js`)) return `${required_path}${sep}index.js`;
+            
+            throw(`O arquivo "${requiredName}" não foi encontrado na biblioteca`);
+         };
 
-         const required_path = normalize(`${requiredResources}${sep}${requiredName}`);
+         const require = exist_require();
 
-         if (_fs.existsSync(`${required_path}${sep}index.js`)) return `${required_path}${sep}index.js`;
-         
-         throw(`O arquivo "${requiredName}" não foi encontrado na biblioteca`);
-      };
-      const require = exist_require();
+         // Check module
+         let current = await fs.readFile(require, 'utf8');
+         current = current.replace(/(\/\*[\s\S]*?\*\/)|(\/{2}.*)|(^\s*$)/gim, '').trim();
 
-      // Check module
-      let current = await fs.readFile(require, 'utf8');
-      current = current.replace(/(\/\*[\s\S]*?\*\/)|(\/{2}.*)|(^\s*$)/gim, '').trim();
+         const outputModule = /(module.exports\s=\s(.*?);)/;
+         const isModule = outputModule.test(current) ? outputModule.exec(current)[2] : false;
 
-      const outputModule = /(module.exports\s=\s(.*?);)/;
-      const isModule = outputModule.test(current) ? outputModule.exec(current)[2] : false;
+         if (typeof isModule !== 'boolean') current = current.replace(RegExp(`const\\s${isModule}\\s=\\s|module.*?;`, 'gm'), '').trim();
 
-      if (typeof isModule !== 'boolean') current = current.replace(RegExp(`const\\s${isModule}\\s=\\s|module.*?;`, 'gm'), '').trim();
+         /* Recursive Library */
+         if (regex.test(current)) current = await recursive_require(require, replace, log);
 
-      /* Recursive Library */
-      if (regex.test(current)) current = await recursive_require(require, replace);
-
-      content = content.replace(requireds[required], current);
+         content = content.replace(requireds[required], current);
+      }
+      catch (e) {
+   
+         log && console.error(e);
+      }
    }
    
    return content;
 }
 
-async function processJS(file, local = false, replace = 'dev') {
+async function processJS(file, local = false, replace = 'dev', log = true) {
 
    const _ = /\.library/.test(file) ? true : false;
 
@@ -89,7 +97,7 @@ async function processJS(file, local = false, replace = 'dev') {
 
    async function pre_process() {
 
-      const content = await recursive_require(file, replace);
+      const content = await recursive_require(file, replace, log);
 
       /* Final Build File */
       await fs.writeFile(pre, content);
