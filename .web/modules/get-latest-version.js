d3af65edf8964d2de9cb1cf479a356203a2d2980
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2021-11-19 22:23:23 -0300

    Dynamic import required packages

---
+++ .web/modules/get-latest-version.js
@@ -0,0 +1,8 @@
+const packageJson = require('package-json');
+const latestVersion = async packageName => {
+   
+   const package = await packageJson(packageName.toLowerCase());
+   return package.version;
+};
+
+module.exports = latestVersion;
--- bin/rebuild-files.js
+++ bin/rebuild-files.js
@@ -1,126 +1,86 @@
 const rebuildFiles = async () => {
 
+   
    const fse = require('fs-extra');
    const exec = require('../.web/modules/execShellCommand');
+   const latest = require('../.web/modules/get-latest-version');
    const { sh, draft } = require('../.web/modules/sh');
    const readJSON = file => JSON.parse(fse.readFileSync(file, 'utf-8'));
    const buildJSON = obj => orderJSON(obj, 2);
-   const package = readJSON('package.json');
-   const babelrc = readJSON('.babelrc');
+   const package = readJSON('package.json') || { };
+   const babelrc = readJSON('.babelrc') || { };
    const stage = {
-   
+      
       package: false,
       babelrc: false,
       error: false,
       npm_i: false
    };
    const orderJSON = (obj, space) => {
-
+      
       const allKeys = [];
       const seen = { };
-
+      
       JSON.stringify(obj, (key, value) => {
-
+         
          if (!(key in seen)) {
-
+            
             allKeys.push(key);
             seen[key] = null;
          }
-
+         
          return value;
       });
-
+      
       allKeys.sort();
       
       return JSON.stringify(obj, allKeys, space);
    };
-   
+   const dependencies = [
+      
+      '@babel/cli',
+      '@babel/core',
+      '@babel/preset-env',
+      'autoprefixer',
+      'postcss-cli',
+      'sass',
+      'uglify-js'
+   ];
+
    /* package.json */
    try {
    
-      /* Dist */
-      if (!package?.dependencies) {
-         
-         package.dependencies = { };
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
+      if (!package?.dependencies) package.dependencies = { };
+      
+      for (const dependence of dependencies) {
+
+         if (!package?.dependencies[dependence]) {
+
+            package.dependencies[dependence] = `^${await latest(dependence)}`;
+            
+            if (!stage.package) stage.package = true;
+            if (!stage.npm_i) stage.npm_i = true;
+         }
       }
 
-      // javascript (babel, uglify-js)
-      if (!package?.dependencies['@babel/cli']) {
-         
-         package.dependencies['@babel/cli'] = '^7.16.0';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-      if (!package?.dependencies['@babel/core']) {
-         
-         package.dependencies['@babel/core'] = '^7.16.0';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-      if (!package?.dependencies['@babel/preset-env']) {
-         
-         package.dependencies['@babel/preset-env'] = '^7.16.0';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-      if (!package?.dependencies['uglify-js']) {
-         
-         package.dependencies['uglify-js'] = '^3.14.3';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-   
-      // css (autoprefixer, post-cli, sass)
-      if (!package?.dependencies.autoprefixer) {
-         
-         package.dependencies.autoprefixer = '^10.4.0';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
+      /* autoprefixer requires */
       if (!package?.browserslist) {
    
          package.browserslist = '> 0%';
          if (!stage.package) stage.package = true;
       }
-      if (!package?.dependencies['postcss-cli']) {
-   
-         package.dependencies['postcss-cli'] = '^9.0.1';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-      if (!package?.dependencies['sass']) {
-   
-         package.dependencies['sass'] = '^1.43.4';
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
 
       /* Dev */
-      if (!package?.devDependencies) {
-         
-         package.devDependencies = { };
-         if (!stage.package) stage.package = true;
-         if (!stage.npm_i) stage.npm_i = true;
-      }
-   
+      if (!package?.devDependencies) package.devDependencies = { };
       if (!package?.devDependencies?.web) {
          
          package.devDependencies.web = 'file:.library';
+
          if (!stage.package) stage.package = true;
          if (!stage.npm_i) stage.npm_i = true;
       }
    
       if (stage.package) fse.writeFileSync('package.json', buildJSON(package));
-   } catch (error) {
-   
-      console.warn('Unable to get the needed resources into package.json.\nPlease, look at: https://github.com/wellwelwel/simple-web/blob/main/package.json and insert "browserslist" and local dependence "web" manually\n');
-      console.error(`Error: ${error.message}\n`);
-   
-      if (!stage.error) stage.error = true;
-   } finally {
-   
       if (stage.npm_i) {
          
          console.log(sh.clear);
@@ -130,6 +90,13 @@ const rebuildFiles = async () => {
          await exec('npm i');
          importing.stop(1);
       }
+   }
+   catch (error) {
+   
+      console.warn('Unable to get the needed resources into package.json.\nPlease, look at: https://github.com/wellwelwel/simple-web/blob/main/package.json and insert "browserslist" and local dependence "web" manually\n');
+      console.error(`Error: ${error.message}\n`);
+   
+      if (!stage.error) stage.error = true;
    }
    
    /* .babelrc */
