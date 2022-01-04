5ea06a9daea17c5132b632f0c16044d3c87454b8
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-01-04 02:27:33 -0300

    Checking modules compatibility

--- bin/init.js
+++ bin/init.js
@@ -1,14 +1,19 @@
 #! /usr/bin/env node
 
-var isNodeCompatible = require('../bin/is-node-compatible.js');
-var isNPMCompatible = require('../bin/is-npm-compatible.js');
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
-if (isNodeCompatible() !== true) {
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-   console.log("> The minimum required Node.js version is 14.15. Please upgrade to continue.");
-   console.log();
-} else if (isNPMCompatible() !== true) {
+_asyncToGenerator(function* () {
+  var isNodeCompatible = require('../bin/is-node-compatible.js');
 
-   console.log("> The minimum required NPM version is 7.0.2. Please upgrade to continue.");
-   console.log();
-} else require("../bin/process-resources");
+  var isNPMCompatible = require('../bin/is-npm-compatible.js');
+
+  if (isNodeCompatible() !== true) {
+    console.log("> The minimum required Node.js version is 14.15. Please upgrade to continue.");
+    console.log();
+  } else if ((yield isNPMCompatible()) !== true) {
+    console.log("> The minimum required NPM version is 7.0.2. Please upgrade to continue.");
+    console.log();
+  } else require("../bin/process-resources");
+})();
