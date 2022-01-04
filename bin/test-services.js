15bd638f77168ccfbcdc60517ac9e1aa04fd3067
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-01-04 00:16:53 -0300

    Checking modules compatibility

--- bin/test-services.js
+++ bin/test-services.js
@@ -51,9 +51,9 @@
             await sh('npm i');
 
             console.log('   ➕  <=14 NodeJS: Downgrade dependencies...');
-            await sh('npm i postcss-cli@8.3.1 --force');
             await sh('npm i autoprefixer@6.7.7 --force');
             await sh('npm i globby@11.0.4 --force');
+            await sh('npm i postcss-cli@8.3.1 --force');
 
             console.log('   ➕ Linking service...');
             await sh('npm link');
@@ -70,9 +70,9 @@
          if (+process.version.split(".").shift().replace(/[^0-9]/, '') <= 14) {
 
             console.log('   ➕  <=14 NodeJS: Downgrade dependencies...');
-            await sh('cd "temp" && npm i postcss-cli@8.3.1 --force');
             await sh('cd "temp" && npm i autoprefixer@6.7.7 --force');
             await sh('cd "temp" && npm i globby@11.0.4 --force');
+            await sh('cd "temp" && npm i postcss-cli@8.3.1 --force');
          }
 
          let start_errors = 0;
