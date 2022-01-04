dac705684a9f9ca1ee12e513c8d3636d3b60e33e
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-01-04 01:40:51 -0300

    Checking modules compatibility

--- bin/init.js
+++ bin/init.js
@@ -5,5 +5,6 @@ var isCompatible = require('../bin/is-compatible.js');
 if (isCompatible() !== true) {
 
    console.log("> The minimum required Node.js version is 14.15. Please upgrade to continue.");
+   console.log("> The minimum required npm version is 7.0.2. Please upgrade to continue.");
    console.log();
 } else require("../bin/process-resources");
