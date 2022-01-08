f1efc0914cd717fdb993387a08a62b4d54313a54
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-01-08 01:52:31 -0300

    Disabling FTP test temporarily

--- bin/test-services.js
+++ bin/test-services.js
@@ -4,28 +4,28 @@
    const fs = require('fs');
    const { extname } = require('path');
    const sh = async command => new Promise((resolve, reject) => exec(command, (error, stdout) => !!error ? reject(error) : resolve(stdout)));
-   const readJSON = file => JSON.parse(fs.readFileSync(file, 'utf-8'));
-   const buildJSON = obj => orderJSON(obj, 2);
-   const orderJSON = (obj, space) => {
+   // const readJSON = file => JSON.parse(fs.readFileSync(file, 'utf-8'));
+   // const buildJSON = obj => orderJSON(obj, 2);
+   // const orderJSON = (obj, space) => {
 
-      const allKeys = [];
-      const seen = { };
+   //    const allKeys = [];
+   //    const seen = { };
 
-      JSON.stringify(obj, (key, value) => {
+   //    JSON.stringify(obj, (key, value) => {
 
-         if (!(key in seen)) {
+   //       if (!(key in seen)) {
 
-            allKeys.push(key);
-            seen[key] = null;
-         }
+   //          allKeys.push(key);
+   //          seen[key] = null;
+   //       }
 
-         return value;
-      });
+   //       return value;
+   //    });
 
-      allKeys.sort();
+   //    allKeys.sort();
 
-      return JSON.stringify(obj, allKeys, space);
-   };
+   //    return JSON.stringify(obj, allKeys, space);
+   // };
    const pass = (stdout, regex = /PASSED/gm) => regex.test(stdout);
    const results = {
 
@@ -212,29 +212,29 @@
    }
 
    /* Testing FTP service */
-   if (process.platform === "linux") {
+   // if (process.platform === "linux") {
 
-      console.log('➖ Testing FTP service...');
+   //    console.log('➖ Testing FTP service...');
 
-      const web_config = readJSON('temp/.web-config.json');
+   //    const web_config = readJSON('temp/.web-config.json');
 
-      web_config.dev.ftp.root = '/';
-      web_config.dev.ftp.host = '127.0.0.1';
-      web_config.dev.ftp.user = 'test';
-      web_config.dev.ftp.pass = 'test';
-      web_config.dev.ftp.secure = 'explict';
+   //    web_config.dev.ftp.root = '/';
+   //    web_config.dev.ftp.host = '127.0.0.1';
+   //    web_config.dev.ftp.user = 'test';
+   //    web_config.dev.ftp.pass = 'test';
+   //    web_config.dev.ftp.secure = 'explict';
 
-      fs.writeFileSync('temp/.web-config.json', buildJSON(web_config));
+   //    fs.writeFileSync('temp/.web-config.json', buildJSON(web_config));
 
-      setTimeout(() => sh('cd "temp" && touch "src/exit"'), 5000);
+   //    setTimeout(() => sh('cd "temp" && touch "src/exit"'), 5000);
 
-      const FTP = await sh('cd "temp" && simple-web --TEST');
-      const passed = pass(FTP, /Connected/gm);
+   //    const FTP = await sh('cd "temp" && simple-web --TEST');
+   //    const passed = pass(FTP, /Connected/gm);
 
-      if (!passed) errors.push({ 'Testing FTP service:': FTP });
+   //    if (!passed) errors.push({ 'Testing FTP service:': FTP });
 
-      console.log(results[passed ? 'passed' : 'failed']);
-   }
+   //    console.log(results[passed ? 'passed' : 'failed']);
+   // }
 
    if (fs.existsSync('temp')) {
 
