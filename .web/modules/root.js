6b3fbd96962ffb05748ededa1d0ffb99caf7dba4
wellwelwel <46850407+wellwelwel@users.noreply.github.com>
2022-12-19 23:05:56 -0300

    Fixing Windows compatibility

--- .web/modules/root.js
+++ .web/modules/root.js
@@ -4,9 +4,9 @@ import { platform } from 'os';
 const isWindows = platform() === 'win32';
 
 export const __dirname = (() => {
-   const x = dirname(decodeURI(new URL(import.meta.url).pathname));
-   const currentPath = isWindows ? x.substring(1) : x;
-   const paths = currentPath.split(sep);
+   const meta = dirname(decodeURI(new URL(import.meta.url).pathname));
+   const currentPath = isWindows ? meta.substring(1) : meta;
+   const paths = currentPath.split('/');
    const rootIndex = paths.lastIndexOf('simple-web-cli');
 
    return resolve(paths.splice(0, rootIndex + 1).join(sep));
